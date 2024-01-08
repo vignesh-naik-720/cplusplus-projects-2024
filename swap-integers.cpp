f3a7bb93c253b231882799c14811dcf4a52182f2
Vignesh Naik <148850345+vignesh-naik-720@users.noreply.github.com>
2024-01-08 21:49:16 +0530

    Create swap-integers.cpp

---
+++ swap-integers.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+// Driver code
+int main()
+{
+    int a = 2, b = 3;
+ 
+    cout << "Before swapping a = " << a << " , b = " << b
+         << endl;
+ 
+    // temporary variable
+    int temp;
+ 
+    // appying swapping algorithm
+    temp = a;
+    a = b;
+    b = temp;
+    cout << "After swapping a = " << a << " , b = " << b
+         << endl;
+ 
+    return 0;
+}
