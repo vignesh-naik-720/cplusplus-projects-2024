abcdca950863182fdd5d21faa66fb1c82c57041b
Vignesh Naik <148850345+vignesh-naik-720@users.noreply.github.com>
2024-01-09 21:15:00 +0530

    Create simple-interest.cpp

---
+++ simple-interest.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    float P = 1, R = 1, T = 1;
+    float SI = (P * T * R) / 100;
+    cout << "Simple Interest = " << SI;
+    return 0;
+}
